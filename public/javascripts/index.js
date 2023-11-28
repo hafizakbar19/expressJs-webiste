7bef11189f1fa73f1b01336d3fe8e8a4650a451a
Mohammed Akbar Ali <96139030+hafizakbar19@users.noreply.github.com>
2023-11-28 23:28:46 +0500

    express static practice

---
+++ public/javascripts/index.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var navMenu = document.getElementById("mobile-menu-js");
+    if (navMenu.style.height == "0px") {
+      navMenu.style.height = "134px";     
+    } else {
+      navMenu.style.height = "0px";                  
+    }
+  }
