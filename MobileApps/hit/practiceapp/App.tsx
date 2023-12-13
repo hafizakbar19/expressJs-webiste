79d133ea059132606479795e0ce369cb40cabad7
Mohammed Akbar Ali <96139030+hafizakbar19@users.noreply.github.com>
2023-12-13 15:39:06 +0500

    styles

--- MobileApps/hit/practiceapp/App.tsx
+++ MobileApps/hit/practiceapp/App.tsx
@@ -17,7 +17,7 @@ function App(){
       <View style={styles.container}>
         <Text style={isDarkMode ? styles.whiteText : styles.blackText}>Hello world!</Text>
         <Text>Welcome to my new App</Text>
-        <Pressable><Text style={isDarkMode ? styles.whiteText : styles.blackText}>Click me!</Text></Pressable>
+        <Pressable style={styles.button}><Text style={{color: 'white'}}>Click me!</Text></Pressable>
       </View>
   )
 }
@@ -34,6 +34,12 @@ const styles = StyleSheet.create({
   },
   blackText: {
     color: '#000000'
+  },
+  button: {
+    backgroundColor: 'blue',
+    borderRadius: 10,
+    margin: 5,
+    padding: 5
   }
 })
 
