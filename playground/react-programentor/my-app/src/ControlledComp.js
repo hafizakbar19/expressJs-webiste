e4fcf5f9635b368d2f0a4b6a6798000e4bfa9719
Mohammed Akbar Ali <96139030+hafizakbar19@users.noreply.github.com>
2023-12-13 23:35:47 +0500

    controlled components in react forms practice

--- playground/react-programentor/my-app/src/ControlledComp.js
+++ playground/react-programentor/my-app/src/ControlledComp.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react'
 
 function ControlledComp() {
-    const [name, setName] = useState('Akbar');
+    const [name, setName] = useState('');
+
+    const changeHandler = (e) => {
+      setName(
+        e.target.value
+      )
+      console.log(e.target)
+    }
 
   return (
     <div>
+        <h1>{name}</h1>
         <form>
             <label htmlFor='userName'>Enter Name: </label>
             <br />
-            <input type='text' name='userName' value={name} onChange={}/>
+            <input type='text' name='userName' value={name} onChange={changeHandler}/>
             <br />
             <input type='submit' value='Submit' />
         </form>
