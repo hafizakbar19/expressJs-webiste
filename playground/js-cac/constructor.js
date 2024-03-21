63dff3b0b0d74db3b1b2f3111b3c8f0aaa372c59
Mohammed Akbar Ali <96139030+hafizakbar19@users.noreply.github.com>
2024-03-22 00:09:48 +0500

    constructor lecture covered

---
+++ playground/js-cac/constructor.js
@@ -0,0 +1,15 @@
+function User (userName, age, city, isLoggedIn){
+    this.userName = userName,
+    this.age = age,
+    this.city = city,
+    this.isLoggedIn = isLoggedIn,
+
+    this.greeting = function(){
+        console.log(`hello ${this.userName}`)
+    }
+}
+
+let user1 = new User("ali",25,"Karachi", true);
+let user2 = new User("Khan",28,"Lahore", false);
+
+console.log(user2.city)
