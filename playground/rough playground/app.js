7672b60315a7e79d41b7e40bd2d496dd31c38cb7
Mohammed Akbar Ali <96139030+hafizakbar19@users.noreply.github.com>
2024-04-06 13:06:00 +0500

    scope play

--- playground/rough playground/app.js
+++ playground/rough playground/app.js
@@ -1,11 +1,20 @@
-const button = document.querySelector('button');
-const dice = document.getElementById('dice');
+// const button = document.querySelector('button');
+// const dice = document.getElementById('dice');
 
 
-const clickHandler = () => {
-    let value = Math.floor(Math.random()*6 +1);
-        dice.innerHTML = value;
+// const clickHandler = () => {
+//     let value = Math.floor(Math.random()*6 +1);
+//         dice.innerHTML = value;
 
-};
+// };
 
-button.onclick = clickHandler;
+// button.onclick = clickHandler;
+
+
+// const x = 10;
+
+// if(true){
+//     const x = 20;
+//     console.log(x)
+// }
+// console.log(x)
